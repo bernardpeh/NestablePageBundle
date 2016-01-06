0f156dbac6d786d000a45d10d42583e1251a16f6
Bernard Peh <bernard.peh@cheproximity.com.au>
2016-01-06 17:05:22 +1100

    refactor classes

---
+++ Model/PageBase.php
@@ -0,0 +1,327 @@
+<?php
+
+namespace Bpeh\NestablePageBundle\Model;
+
+use Doctrine\ORM\Mapping as ORM;
+
+/**
+ * Page
+ *
+ * @ORM\Table()
+ * @ORM\Entity(repositoryClass="Bpeh\NestablePageBundle\Entity\PageRepository")
+ * @ORM\HasLifecycleCallbacks()
+ * @ORM\InheritanceType("SINGLE_TABLE")
+ * @ORM\DiscriminatorColumn(name="discr", type="string")
+ * @ORM\DiscriminatorMap({"pagebase" = "PageBase", "page" = "Bpeh\NestablePageBundle\Entity\Page"})
+ */
+abstract class PageBase
+{
+    /**
+     * @var integer
+     *
+     * @ORM\Column(name="id", type="integer")
+     * @ORM\Id
+     * @ORM\GeneratedValue(strategy="AUTO")
+     */
+    protected $id;
+
+    /**
+     * @var string
+     *
+     * @ORM\Column(name="slug", type="string", length=255, unique=true)
+     */
+    protected $slug;
+
+    /**
+     * @var boolean
+     *
+     * @ORM\Column(name="isPublished", type="boolean", nullable=true)
+     */
+    protected $isPublished;
+
+    /**
+     * @var integer
+     *
+     * @ORM\Column(name="sequence", type="integer", nullable=true)
+     */
+    protected $sequence;
+
+    /**
+     * @var \DateTime
+     *
+     * @ORM\Column(name="modified", type="datetime")
+     */
+    protected $modified;
+
+    /**
+     * @var \DateTime
+     *
+     * @ORM\Column(name="created", type="datetime")
+     */
+    protected $created;
+
+
+    /**
+     * @ORM\ManyToOne(targetEntity="Bpeh\NestablePageBundle\Model\PageBase", inversedBy="children")
+     * @ORM\JoinColumn(name="parent_id", referencedColumnName="id", onDelete="CASCADE")}
+     * @ORM\OrderBy({"sequence" = "ASC"})
+     */
+    protected $parent;
+
+    /**
+     * @ORM\OneToMany(targetEntity="Bpeh\NestablePageBundle\Model\PageBase", mappedBy="parent")
+     * @ORM\OrderBy({"sequence" = "ASC"})
+     */
+    protected $children;
+   
+    /**
+     * @ORM\OneToMany(targetEntity="Bpeh\NestablePageBundle\Model\PageMetaBase", mappedBy="page", cascade={"persist"}))
+     */
+    protected $pageMetas;
+    
+    /**
+     * Get id
+     *
+     * @return integer 
+     */
+    public function getId()
+    {
+        return $this->id;
+    }
+
+    /**
+     * Set slug
+     *
+     * @param string $slug
+     * @return Page
+     */
+    public function setSlug($slug)
+    {
+        $this->slug = $slug;
+
+        return $this;
+    }
+
+    /**
+     * Get slug
+     *
+     * @return string 
+     */
+    public function getSlug()
+    {
+        return $this->slug;
+    }
+
+    /**
+     * Set isPublished
+     *
+     * @param boolean $isPublished
+     * @return Page
+     */
+    public function setIsPublished($isPublished)
+    {
+        $this->isPublished = $isPublished;
+
+        return $this;
+    }
+
+    /**
+     * Get isPublished
+     *
+     * @return boolean 
+     */
+    public function getIsPublished()
+    {
+        return $this->isPublished;
+    }
+
+    /**
+     * Set sequence
+     *
+     * @param integer $sequence
+     * @return Page
+     */
+    public function setSequence($sequence)
+    {
+        $this->sequence = $sequence;
+
+        return $this;
+    }
+
+    /**
+     * Get sequence
+     *
+     * @return integer 
+     */
+    public function getSequence()
+    {
+        return $this->sequence;
+    }
+
+    /**
+     * Set modified
+     *
+     * @param \DateTime $modified
+     * @return Page
+     */
+    public function setModified($modified)
+    {
+        $this->modified = $modified;
+
+        return $this;
+    }
+
+    /**
+     * Get modified
+     *
+     * @return \DateTime 
+     */
+    public function getModified()
+    {
+        return $this->modified;
+    }
+
+    /**
+     * Set created
+     *
+     * @param \DateTime $created
+     * @return Page
+     */
+    public function setCreated($created)
+    {
+        $this->created = $created;
+
+        return $this;
+    }
+
+    /**
+     * Get created
+     *
+     * @return \DateTime 
+     */
+    public function getCreated()
+    {
+        return $this->created;
+    }
+    /**
+     * Constructor
+     */
+    public function __construct()
+    {
+        $this->children = new \Doctrine\Common\Collections\ArrayCollection();
+        $this->pageMetas = new \Doctrine\Common\Collections\ArrayCollection();
+    }
+
+    /**
+     * @ORM\PrePersist
+     */
+    public function prePersist()
+    {
+        // update the modified time
+        $this->setModified(new \DateTime());
+
+        // for newly created entries
+        if ($this->getCreated() == null) {
+            $this->setCreated(new \DateTime('now'));
+        }
+        $this->created = new \DateTime();
+    }
+
+    /**
+     * Set parent
+     *
+     * @param \Bpeh\NestablePageBundle\Model\PageBase $parent
+     * @return Page
+     */
+    public function setParent(\Bpeh\NestablePageBundle\Model\PageBase $parent = null)
+    {
+        $this->parent = $parent;
+
+        return $this;
+    }
+
+    /**
+     * Get parent
+     *
+     * @return \Bpeh\NestablePageBundle\Model\PageBase 
+     */
+    public function getParent()
+    {
+        return $this->parent;
+    }
+
+    /**
+     * Add children
+     *
+     * @param \Bpeh\NestablePageBundle\Model\PageBase $children
+     * @return Page
+     */
+    public function addChild(\Bpeh\NestablePageBundle\Model\PageBase $children)
+    {
+        $this->children[] = $children;
+
+        return $this;
+    }
+
+    /**
+     * Remove children
+     *
+     * @param \Bpeh\NestablePageBundle\Model\Page $children
+     */
+    public function removeChild(\Bpeh\NestablePageBundle\Model\PageBase $children)
+    {
+        $this->children->removeElement($children);
+    }
+
+    /**
+     * Get children
+     *
+     * @return \Doctrine\Common\Collections\Collection 
+     */
+    public function getChildren()
+    {
+        return $this->children;
+    }
+
+    /**
+     * Add pageMetas
+     *
+     * @param \Bpeh\NestablePageBundle\Model\PageMetaBase $pageMetas
+     * @return Page
+     */
+    public function addPageMeta(\Bpeh\NestablePageBundle\Model\PageMetaBase $pageMetas)
+    {
+        $this->pageMetas[] = $pageMetas;
+
+        return $this;
+    }
+
+    /**
+     * Remove pageMetas
+     *
+     * @param \Bpeh\NestablePageBundle\Model\PageMetaBase $pageMetas
+     */
+    public function removePageMeta(\Bpeh\NestablePageBundle\Model\PageMetaBase $pageMetas)
+    {
+        $this->pageMetas->removeElement($pageMetas);
+    }
+
+    /**
+     * Get pageMetas
+     *
+     * @return \Doctrine\Common\Collections\Collection 
+     */
+    public function getPageMetas()
+    {
+        return $this->pageMetas;
+    }
+    
+    /**
+     * convert object to string
+     * @return string
+     */
+    public function __toString()
+    {
+        return $this->slug;
+    }
+}
---
+++ Model/PageMetaBase.php
@@ -0,0 +1,226 @@
+<?php
+
+namespace Bpeh\NestablePageBundle\Model;
+
+use Doctrine\ORM\Mapping as ORM;
+
+/**
+ * PageMeta
+ *
+ * @ORM\Table()
+ * @ORM\Entity
+ * @ORM\InheritanceType("SINGLE_TABLE")
+ * @ORM\DiscriminatorColumn(name="discr", type="string")
+ * @ORM\DiscriminatorMap({"pagemetabase" = "PageMetaBase", "pagemeta" = "Bpeh\NestablePageBundle\Entity\PageMeta"})
+ */
+abstract class PageMetaBase
+{
+    /**
+     * @var integer
+     *
+     * @ORM\Column(name="id", type="integer")
+     * @ORM\Id
+     * @ORM\GeneratedValue(strategy="AUTO")
+     */
+    protected $id;
+
+    /**
+     * @var string
+     *
+     * @ORM\Column(name="page_title", type="string", length=255)
+     */
+    protected $page_title;
+
+    /**
+     * @var string
+     *
+     * @ORM\Column(name="menu_title", type="string", length=255)
+     */
+    protected $menu_title;
+
+    /**
+     * @var string
+     *
+     * @ORM\Column(name="locale", type="string", length=4)
+     */
+    protected $locale;
+
+    /**
+     * @var string
+     *
+     * @ORM\Column(name="short_description", type="text", nullable=true)
+     */
+    protected $short_description;
+
+    /**
+     * @var string
+     *
+     * @ORM\Column(name="content", type="text", nullable=true)
+     */
+    protected $content;
+
+    /**
+     * @ORM\ManyToOne(targetEntity="Bpeh\NestablePageBundle\Model\PageBase", inversedBy="pageMetas")
+     * @ORM\JoinColumn(name="page_id", referencedColumnName="id", onDelete="CASCADE")}
+     */
+    protected $page;
+
+    /**
+     * constructor
+     */
+    public function __construct()
+    {
+        // default values
+        $this->locale = 'en';
+    }
+
+    /**
+     * Get id
+     *
+     * @return integer 
+     */
+    public function getId()
+    {
+        return $this->id;
+    }
+
+    /**
+     * Set page_title
+     *
+     * @param string $pageTitle
+     * @return PageMeta
+     */
+    public function setPageTitle($pageTitle)
+    {
+        $this->page_title = $pageTitle;
+
+        return $this;
+    }
+
+    /**
+     * Get page_title
+     *
+     * @return string 
+     */
+    public function getPageTitle()
+    {
+        return $this->page_title;
+    }
+
+    /**
+     * Set menu_title
+     *
+     * @param string $menuTitle
+     * @return PageMeta
+     */
+    public function setMenuTitle($menuTitle)
+    {
+        $this->menu_title = $menuTitle;
+
+        return $this;
+    }
+
+    /**
+     * Get menu_title
+     *
+     * @return string 
+     */
+    public function getMenuTitle()
+    {
+        return $this->menu_title;
+    }
+
+    /**
+     * Set locale
+     *
+     * @param string $locale
+     * @return PageMeta
+     */
+    public function setLocale($locale)
+    {
+        $this->locale = $locale;
+
+        return $this;
+    }
+
+    /**
+     * Get locale
+     *
+     * @return string 
+     */
+    public function getLocale()
+    {
+        return $this->locale;
+    }
+
+    /**
+     * Set short_description
+     *
+     * @param string $shortDescription
+     * @return PageMeta
+     */
+    public function setShortDescription($shortDescription)
+    {
+        $this->short_description = $shortDescription;
+
+        return $this;
+    }
+
+    /**
+     * Get short_description
+     *
+     * @return string 
+     */
+    public function getShortDescription()
+    {
+        return $this->short_description;
+    }
+
+    /**
+     * Set content
+     *
+     * @param string $content
+     * @return PageMeta
+     */
+    public function setContent($content)
+    {
+        $this->content = $content;
+
+        return $this;
+    }
+
+    /**
+     * Get content
+     *
+     * @return string 
+     */
+    public function getContent()
+    {
+        return $this->content;
+    }
+
+    /**
+     * Set page
+     *
+     * @param \Bpeh\NestablePageBundle\Model\PageBase $page
+     * @return PageMeta
+     */
+    public function setPage(\Bpeh\NestablePageBundle\Model\PageBase $page = null)
+    {
+        $this->page = $page;
+
+        return $this;
+    }
+
+    /**
+     * Get page
+     *
+     * @return \Bpeh\NestablePageBundle\Model\PageBase
+     */
+    public function getPage()
+    {
+        return $this->page;
+    }
+
+    
+}
